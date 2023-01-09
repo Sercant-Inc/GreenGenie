5b99c97f07ffabf3e7e745bfb922237a2e9838fe
Sergio Canas <sergiocanas@outlook.com>
2023-01-09 11:52:16 +0100

    Added anim

--- app/src/main/java/com/sergio/greengenie/Splash.java
+++ app/src/main/java/com/sergio/greengenie/Splash.java
@@ -3,13 +3,19 @@ package com.sergio.greengenie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
-
+    ImageView genio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        genio=findViewById(R.id.geniosplash);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.splash_anim);
+        genio.startAnimation(myanim);
 
     }
 }

da2d87cfe206d47d5cd150e49ae93a0162efbc98
Antonio Soto Cid <asotocid@gmail.com>
2023-01-09 12:05:22 +0100

    Intent to RegisterPage

--- app/src/main/java/com/sergio/greengenie/RegisterPage.java
+++ app/src/main/java/com/sergio/greengenie/RegisterPage.java
@@ -2,6 +2,7 @@ package com.sergio.greengenie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class RegisterPage extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class RegisterPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+        Intent intent2 = getIntent();
     }
 }

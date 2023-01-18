5838d896bfc8c5c8f6db04f53272267106dab34a
Carlos Perez <carlosavenidaeuro@gmail.com>
2023-01-18 21:50:26 +0100

    Terms & conditions in policy xml design. Some adjust in login page and icon app fixed

--- app/src/main/java/com/sergio/greengenie/LoginPage.java
+++ app/src/main/java/com/sergio/greengenie/LoginPage.java
@@ -6,16 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
     Button button2;
     Button button1;
+    TextView textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
         button1=findViewById(R.id.button1);
         button2=findViewById(R.id.button2);
+        textView2=findViewById(R.id.textView2);
 
 
 
@@ -33,6 +36,12 @@ public class LoginPage extends AppCompatActivity {
             }
 
         });
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPolicy();
+            }
+        });
     }
     public void openRegister(){
         Intent intent = new Intent(this, RegisterPage.class);
@@ -45,6 +54,11 @@ public class LoginPage extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
+        startActivity(intent);
+    }
+    public void openPolicy(){
+
+        Intent intent = new Intent(this, PolicyActivity.class);
         startActivity(intent);
     }
 }

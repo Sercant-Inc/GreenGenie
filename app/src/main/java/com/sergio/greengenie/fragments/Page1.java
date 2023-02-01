a594278a6682c13b8e32d61545d4783157ed8326
Carlos Perez <carlosavenidaeuro@gmail.com>
2023-02-01 10:44:38 +0100

    page 1 in fragment working. ALL TIPS ADDED

--- app/src/main/java/com/sergio/greengenie/fragments/Page1.java
+++ app/src/main/java/com/sergio/greengenie/fragments/Page1.java
@@ -1,6 +1,5 @@
 package com.sergio.greengenie.Fragments;
 
-import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,21 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.sergio.greengenie.R;
-
-import android.content.Intent;
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.sergio.greengenie.PolicyActivity;
 import com.sergio.greengenie.R;
 
 /**
@@ -56,7 +40,6 @@ public class Page1 extends Fragment {
      */
     // TODO: Rename and change types and number of parameters
     public static Page1 newInstance(String param1, String param2) {
-
         Page1 fragment = new Page1();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
@@ -67,44 +50,17 @@ public class Page1 extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-
         }
-
-
-
     }
-//    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-//}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_page1, container, false);
-
-        // Get references to the ImageView and TextView
-        ImageView imageView3 = view.findViewById(R.id.imageView3);
-        final TextView textView = view.findViewById(R.id.textView);
-        Resources res= getResources();
-        String[] genieTips;
-        genieTips= res.getStringArray(R.array.tips);
-        // Set an OnClickListener on the ImageView
-        imageView3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Change the text of the TextView
-                int random=(int)(Math.random()* genieTips.length);
-                textView.setText(genieTips[random]);
-
-            }
-        });
-
-        return view;
+        return inflater.inflate(R.layout.fragment_page1, container, false);
     }
 }
